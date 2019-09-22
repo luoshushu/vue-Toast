277541c0054df97a6856a9f28cedc0eda2a51077
luoshushu <852835916@qq.com>
2019-09-22 14:14:07 +0800

    实现自动关闭

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,33 +1,56 @@
 <template>
   <div class="toast">
-      <slot></slot>  
+    <slot></slot>
   </div>
 </template>
 <script>
-
 export default {
-  name:'Toast'  
-}
+  name: "Toast",
+  props: {
+    // 自动关闭
+    autoClose: {
+      type: Boolean,
+      default: true
+    },
+    // 关闭时间
+    autoCloseDelay: {
+      type: Number,
+      default: 5
+    }
+  },
+  mounted() {
+    if (this.autoClose) {
+      setTimeout(() => {
+        this.close();
+      }, this.autoCloseDelay * 1000);
+    }
+  },
+  methods: {
+    close() {
+      this.$el.remove(); //删除
+      this.$destroy(); //清除绑定的一些事件
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
- $font-size:14px;
-  $toast-height:40px;
-  $toast-bg:rgba(0, 0, 0, 0.75);
-  .toast {
-    font-size: $font-size;
-    line-height: 1.8;
-    height: $toast-height;
-    position: fixed;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    display: flex;
-    align-items: center;
-    color: white;
-    background: $toast-bg;
-    border-radius: 4px;
-    box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.50);
-    padding: 0 16px;
-   
-  }
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.75);
+.toast {
+  font-size: $font-size;
+  line-height: 1.8;
+  height: $toast-height;
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  align-items: center;
+  color: white;
+  background: $toast-bg;
+  border-radius: 4px;
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
+  padding: 0 16px;
+}
 </style>
